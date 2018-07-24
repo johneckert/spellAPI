37b19804e41e4c324c4757945f7ab51dc543b8b6
John Eckert <johnteckert@gmail.com>
2018-07-24 14:20:36 -0400

    updated allowed CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://johneckert.github.io/DnD5eSpellList/', 'http://localhost:3001']
+    origins ['https://johneckert.github.io']
 
     resource '*',
       headers: :any,
