141ecd8b988a1430b071a4956f68512b190e8dd5
John Eckert <johnteckert@gmail.com>
2018-07-24 11:02:46 -0400

    setup controller methods

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,2 +1,11 @@
 class SpellsController < ApplicationController
+  def index
+    @spells = Spell.all
+    render json: @spells, status: 200
+  end
+
+  def show
+    @spells = Spell.get_by_level params[:id].to_i
+    render json: @spells, status: 200
+  end
 end
