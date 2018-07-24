4812e6f07277971edb4b0a12c49f516f7abec276
John Eckert <johnteckert@gmail.com>
2018-07-24 10:48:38 -0400

    added get by level

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,2 +1,12 @@
 class Spell < ApplicationRecord
+
+  def self.get_by_level lvl
+    spells_of_lvl = []
+    self.all.each do |spell|
+      if spell.level == lvl
+        spells_of_lvl << spell
+      end
+    end
+    spells_of_lvl
+  end
 end
